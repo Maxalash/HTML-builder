c01824d6c94c7c0a6aa009ec6bc6b0ae0ebb1ed5
Makhmut <mahmudlyder@gmail.com>
2022-05-22 22:46:11 +0600

    feat: add program to show files recursively

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout} = process;
+
+const readFrom = path.join(__dirname, 'secret-folder'); // из какой папки нужно всё прочитать
+
+function listObjects(path2){
+   fs.readdir(path2, (err, files) => {
+      if(err) throw err;
+        let path1;
+      for (let file of files){
+        path1 = path.join(path2, file)
+        fs.stat(path1, (err, stats) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            if(stats.isDirectory()){
+                path1 = path.join(path2, file)
+                listObjects(path1);
+             }else{
+                 const size=Math.round(stats.size / 1.024)/1000;
+                 const fdata = file.split('.')
+                console.log(fdata[0]+" - "+fdata[1]+" - "+size+'kB');
+             }
+          });
+      }
+   });
+}
+
+listObjects(readFrom);
